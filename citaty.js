d1a0925a0f72835b2dc4f6fd0647ab8d4545791c
Fluscus <brichacekbeda@gmail.com>
2026-08-26 17:30:04 +0200

    Update citaty.js

--- citaty.js
+++ citaty.js
@@ -11,8 +11,8 @@
 
 const citaty = [
   {
-    citat: "Our favourite holding period is forever.",
-    autor: "Warren Buffett",
+    citat: "Planning is important, but the most important part of every plan is to plan on the plan not going according the plan.",
+    autor: "Morgan Housel",
     komentar: "Sem napiš svůj pohled na tenhle citát – proč tě zaujal, souhlasíš s ním, " +
               "nebo si o něm myslíš něco jiného. Klidně ho smaž a nahraď svým vlastním citátem."
   }
