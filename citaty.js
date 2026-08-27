cebd8c9130355584c6a2f7644c4800879e9c3922
Fluscus <brichacekbeda@gmail.com>
2026-08-27 21:10:41 +0200

    Update citaty.js

--- citaty.js
+++ citaty.js
@@ -13,8 +13,10 @@ const citaty = [
   {
     citat: "If you want the rainbow, you gotta put up with the rain.",
     autor: "Dolly Parton",
-    komentar: "Sem napiš svůj pohled na tenhle citát – proč tě zaujal, souhlasíš s ním, " +
-              "nebo si o něm myslíš něco jiného. Klidně ho smaž a nahraď svým vlastním citátem."
+    komentar: "Akciový trh: Z dat plyne, že investování do nízko-nákladových tržně-vážených " +
+              "široce-diverzifikovaných akciových ETF přináší uspokojivý výnos s delším " +
+              "horizontem. Bohužel je to náročná cesta. Investor musí být odolný vůči " +
+              "volatilitě nebo sobě."
   },
   {
     citat: "Planning is important, but the most important part of every plan is to plan on the plan not going according to plan.",
