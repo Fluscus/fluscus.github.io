fcb662ac75be9c8dfdfa8cf33b568dfb2a852558
Fluscus <brichacekbeda@gmail.com>
2026-08-28 09:48:50 +0200

    Update citaty.js

--- citaty.js
+++ citaty.js
@@ -78,7 +78,7 @@ const citaty = [
   {
     citat: "Risk is what's left over when you think you've thought of everything.",
     autor: "Morgan Housel",
-    komentar: "Investování:</br>"+
+    komentar: "Investování:<br>" +
       "Největším rizikem v investování není žádný konkrétní scénář, na který se dá připravit – " +
       "je to samotná nejistota budoucnosti. Nikdo neví přesně, kde bude jakýkoliv trh za rok, " +
       "za pět nebo za dvacet let. Investování je těžké ne proto, že bychom neuměli počítat " +
